f83f05677efb55c0b2ce3d8a14fef874c9193c87
SCo <s.cobano.ed@gmail.com>
2025-03-20 21:02:31 +0100

    write app code for random song selection

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+import pandas as pd
+
+app = Flask(__name__)
+
+#List of random songs
+data = {
+    "title":["Blinding Lights","Shape of You", "Bohemian Rhapsody", "Someone Like You","Uptown Funk"],
+    "artist":["The Weeknd", "Ed Sheeran", "Queen", "Adele", "Mark Ronson ft. Bruno Mars"]
+}
+
+songs = pd.DataFrame(data)
+
+@app.route("/")
+def home():
+    song = df.sample(n=1).iloc[0] #Pick a random song
+    return render_template("index.html", song=song)
+
+
+if __name__=="__main__":
+    app.run(debug=True)
